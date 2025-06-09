037d72c00bfdb6cef185bf26bea7699d25f5af49
alexrv98 <libravarillas777@gmail.com>
2025-06-09 01:17:56 -0600

    Configuración de firebase y organización de carpetas

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+    firebaseConfig: {
+    apiKey: "AIzaSyBZLhErl2kxPEA-kldFw3ahwNRH2t7x31M",
+    authDomain: "sistema-productos-87041.firebaseapp.com",
+    projectId: "sistema-productos-87041",
+    storageBucket: "sistema-productos-87041.firebasestorage.app",
+    messagingSenderId: "279326446913",
+    appId: "1:279326446913:web:c1475b3a7932f28ec2bdc0"
+    
+  }
+  
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from './environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ]
+});
